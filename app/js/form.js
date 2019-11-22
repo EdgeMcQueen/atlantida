37a7f7ec2ba9229309deff2601c7c9472df19f9a
sardor.mutalov <sardor.mutalov@gmail.com>
2019-11-22 18:54:49 +0300

    input fname: проверка валидации - пустое поле, проверка допустимых символов и проверка на нумерные значения

--- app/js/form.js
+++ app/js/form.js
@@ -16,14 +16,30 @@ function validateForm() {
 // Проверка поля name
 function validateName(){
 	let formName = document.forms["mainForm"]["fname"].value;
+	let fname = document.getElementById('fname');
 
 	if (formName.length == 0) {
-		document.getElementById('fname').placeholder = 'Поле обязательно для заполнения';
-    	document.getElementById('fname').style = 'border-color: red;';
+		fname.placeholder = 'Поле обязательно для заполнения';
+    	fname.style = 'border-color: red;';
 		return false;
 	}
+
+	if(/^[a-zA-Z1-9]+$/.test(formName) === false) {
+		alert('В имени должны быть только латинские буквы');
+		fname.style = 'border-color: red;';
+		return false;
+	}
+
+	if(parseInt(formName.substr(0, 1))) {
+		alert('Имя должно начинаться с буквы');
+		fname.style = 'border-color: red;';
+		return false;
+	}
+
   	else {
-		document.getElementById('fname').style = '';
+		fname.style = '';
 		return true;
 	}
 }
+
+
