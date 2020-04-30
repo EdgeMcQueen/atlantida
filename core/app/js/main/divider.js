b8a3ca5f20526efcfc0c561da9b3c627c9f4cebe
SardorMutalov <sardor.mutalov@gmail.com>
2020-04-30 10:54:46 +0300

    project start

---
+++ core/app/js/main/divider.js
@@ -0,0 +1,10 @@
+// подчеркивание заголовка зависит от ширины заголовка
+document.addEventListener('DOMContentLoaded', function() {
+    let headline = document.getElementsByClassName('section-header__headline');
+
+    document.getElementsByClassName('section-header__headline').insertAdjacentHTML('afterend', '<span class="divider">~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~</span>');
+
+    let divider = document.getElementsByClassName('divider');
+    })
+
+
---
+++ core/app/js/main/form.js
@@ -0,0 +1,45 @@
+// отмена перезагрузки страницы при отправке формы
+let eventOff = document.querySelector('#event-off');
+let form = document.querySelector('form');
+
+eventOff.onclick = function (event) {
+	event.preventDefault(); // предотвращает перезагрузку страници при попытке отправить пустую форму
+	console.log(serialize(form));
+}
+
+
+// валидация формы
+
+function validateForm() {
+}
+
+// Проверка поля name
+function validateName(){
+	let formName = document.forms["mainForm"]["fname"].value;
+	let fname = document.getElementById('fname');
+
+	if (formName.length == 0) {
+		fname.placeholder = 'Поле обязательно для заполнения';
+    	fname.style = 'border-color: red;';
+		return false;
+	}
+
+	if(/^[a-zA-Z1-9]+$/.test(formName) === false) {
+		alert('В имени должны быть только латинские буквы');
+		fname.style = 'border-color: red;';
+		return false;
+	}
+
+	if(parseInt(formName.substr(0, 1))) {
+		alert('Имя должно начинаться с буквы');
+		fname.style = 'border-color: red;';
+		return false;
+	}
+
+  	else {
+		fname.style = '';
+		return true;
+	}
+}
+
+
